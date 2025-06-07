ba16d96963ce937724cbfb1c5301914b91d3546b
Dewayne VanHoozer <dvanhoozer@gmail.com>
2025-06-07 15:26:48 -0500

    snapshot

--- examples/llm.rb
+++ examples/llm.rb
@@ -1,6 +1,9 @@
 #!/usr/bin/env ruby
 # examples/llm.rb
 
+require "debug_me"
+include DebugMe
+
 require "shared_tools/llm/run_shell_command"
 
 llm   = LLM.openai(key: ENV["OPENAI_API_KEY"])
@@ -12,4 +15,8 @@ bot.chat "What is the current date?", role: :user
 bot.chat bot.functions.map(&:call) # report return value to the LLM
 
 bot.chat "What operating system am I running? (short version please!)", role: :user
-bot.chat bot.functions.map(&:call) # report return value to the LLM
+response = bot.chat bot.functions.map(&:call) # report return value to the LLM
+
+debug_me{[
+  'response.messages.map(&:content)'
+]}
